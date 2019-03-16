e2a05ae295662035528a25e193ee29f881295c44
Oliver 2wist <joliver017@gmail.com>
2019-03-16 17:55:23 -0500

    cleaned up code and added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,18 +26,20 @@ function start() {
     console.log("Reading all items...\n");
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
+
+        // For each entry in the results obtained from the products database, display item's info
         results.forEach(results=> {
           console.log("Product Name: " + results.product_name + "\n Item ID: " + results.item_id + 
           "\n Department: " + results.department_name + "\n Price: $" + results.price + "\n Current Stock: " + results.stock_quantity + "\n");
         });
         
         promptOption();
-        // connection.end();
     });
 }
 
 function promptOption() { 
   inquirer
+      // This is the first question asked
       .prompt([
           {
           type: "input",
@@ -47,8 +49,9 @@ function promptOption() {
       ])
       .then(function(inquirerResponse1) {
           console.log("Searching item...\n");
+          
+          // Depending on user's chosen input to the first question, the following code will grab the item the user chose
           if (inquirerResponse1.option1) {
-            // console.log(inquirerResponse.option1);
             connection.query(
               "SELECT * FROM products WHERE item_id=?", [inquirerResponse1.option1],
           
@@ -56,6 +59,7 @@ function promptOption() {
                 if (err) throw err;
                 results.forEach(results=> {
                   inquirer
+                      // This is the second question asked
                       .prompt([
                           {
                           type: "input",
@@ -64,22 +68,29 @@ function promptOption() {
                           }
                       ])
                       .then(function(inquirerResponse2) {
+                          // The updatedStock variable lets us define the stock after user buys desired amount (i.e., subtract from current stock)
                           var updatedStock = results.stock_quantity - inquirerResponse2.option2;
+
+                          // The total variable calculates the transaction total (item price * # of units user chose to buy)
+                          // toFixed limits the decimal places to 2
                           var total = (results.price * inquirerResponse2.option2).toFixed(2);
+
                           console.log("Checking stock... " + "We have " + results.stock_quantity + " units left\n");
+
+                          // If the current stock is less than the amount user wants to buy, say there's not enough stock and end the connection to database
                           if (results.stock_quantity < inquirerResponse2.option2) {
                               console.log("Sorry, there's not enough stock.");
                               connection.end();
                           }
+                          // Otherwise, if there is enough stock, the following code will update the stock number of that specific item and display transaction total, and then end the connection
                           else {
-                              if (inquirerResponse2.option2) {
-                              // console.log(inquirerResponse2.option2);                            
+                              if (inquirerResponse2.option2) {                                                    
                               connection.query(
                                 "SELECT * FROM products WHERE item_id=?", [inquirerResponse1.option1],
                             
                               function (err, results) {
                                   if (err) throw err;
-                                  results.forEach(results=> {                                
+                                                                 
                                   connection.query(
                                     "UPDATE products SET stock_quantity=" + updatedStock + " WHERE item_id=" + inquirerResponse1.option1,
                                     function (err, results) {
@@ -89,15 +100,13 @@ function promptOption() {
                                         connection.end();
                                     }
                                 );
+
                                 });
-
-
-                              });
-                            }
+                              }
                           }
                       });
                 });
             });
           }
       });
-}
+};
